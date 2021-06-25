4d262baf69a88332bb2b8b251c95cfd175c6c4dd
fahmi277 <fahmi.sangengon@gmail.com>
2021-06-25 21:24:34 +0700

    add

--- relayControl.py
+++ relayControl.py
@@ -18,7 +18,7 @@ GPIO.setup(relay, GPIO.OUT)
 
 # c.open()
 
-c = ModbusClient(host="192.168.0.7", auto_open=True, auto_close=True,timeout=3)
+c = ModbusClient(host="192.168.0.7", auto_open=True, auto_close=True,timeout=0.5)
 c.port(502)
 c.unit_id(1)
 while(1):
