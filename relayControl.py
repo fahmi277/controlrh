a3c2765bbe818733bb49713edc2e73b50bb061a1
fahmi277 <fahmi.sangengon@gmail.com>
2021-06-25 20:46:06 +0700

    as

--- relayControl.py
+++ relayControl.py
@@ -9,63 +9,44 @@ GPIO.setup(relay, GPIO.OUT)
 
 
 # c = ModbusClient(host="192.168.0.7", auto_open=True, auto_close=True)
-SERVER_HOST = "192.168.0.7"
-SERVER_PORT = 502
+
 c = ModbusClient()
-c.host(SERVER_HOST)
-c.port(SERVER_PORT)
+c.host("192.168.0.7")
 c.port(502)
 c.unit_id(1)
 # managing TCP sessions with call to c.open()/c.close()
 
 # c.open()
 
-while True:
-    # open or reconnect TCP to server
-    if not c.is_open():
-        if not c.open():
-            print("unable to connect to "+SERVER_HOST+":"+str(SERVER_PORT))
-
-    # if open() is ok, read register (modbus function 0x03)
-    if c.is_open():
-        # read 10 registers at address 0, store result in regs list
-        regs = c.read_input_registers(0, 2)
-        # if success display registers
-        if regs:
-            print("reg ad #0 to 9: "+str(regs))
-
-    # sleep 2s before next polling
-    time.sleep(2)
-
-# while(1):
-#     try:
+while(1):
+    try:
 
        
-#         regs = c.read_input_registers(0, 2)
-#         if regs:
-#             print(regs)
+        regs = c.read_input_registers(0, 2)
+        if regs:
+            print(regs)
 
-#             temp =  regs[0]/100
-#             rh = regs[1]/100
+            temp =  regs[0]/100
+            rh = regs[1]/100
 
-#             if rh<75:
-#                 GPIO.output(relay, GPIO.HIGH)
-#                 print("MATI")
+            if rh<75:
+                GPIO.output(relay, GPIO.HIGH)
+                print("MATI")
             
-#             else:
-#                 GPIO.output(relay, GPIO.LOW)
-#                 print("HIDUP")
+            else:
+                GPIO.output(relay, GPIO.LOW)
+                print("HIDUP")
 
 
 
-#         else:
-#             print("read error")
-#             # c.open()
+        else:
+            print("read error")
+            # c.open()
 
-#         time.sleep(2)
+        time.sleep(2)
       
-#     except:
-#          print("read error excep")
+    except:
+         print("read error excep")
         # c.close()
 
 
